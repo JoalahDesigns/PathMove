9289a90d0f0c3f3c668c07077e7801b62ebf81eb
Peter Milway <peter@JoalahDesigns.com>
2012-03-22 10:29:54 -0400

    Initial Commit

---
+++ PathMove/JdAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JdAppDelegate.h
+//  PathMove
+//
+//  Created by Peter Milway on 3/22/12.
+//  Copyright (c) 2012 Joalah Designs LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JdAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PathMove/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PathMove
+//
+//  Created by Peter Milway on 3/22/12.
+//  Copyright (c) 2012 Joalah Designs LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JdAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JdAppDelegate class]));
+    }
+}
